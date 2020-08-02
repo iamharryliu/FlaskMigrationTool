9c1637e5faf6407468bf1d69119e01e88444c34c
Harry <harryliu1995@gmail.com>
2020-08-02 19:31:02 -0400

    remove default bd

--- WebApp/models.py
+++ WebApp/models.py
@@ -9,5 +9,5 @@ class User(db.Model):
     email = db.Column(db.String(50), unique=True, nullable=False)
     phone_number = db.Column(db.String(16))
     signup_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    birth_date = db.Column(db.DateTime, nullable=False, default=datetime(1995, 3, 1))
+    birth_date = db.Column(db.DateTime, nullable=False)
 
