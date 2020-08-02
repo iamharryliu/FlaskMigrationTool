1d19e389900170251d5092dfcfa5257e337c49c3
Harry <harryliu1995@gmail.com>
2020-08-02 14:35:32 -0400

    rename mock script

--- setup_mock_db.py
+++ setup_mock_db.py
@@ -1,24 +1,22 @@
-from time import time
+import time
 from WebApp import create_app, db
-from utils import add_users_to_db
+from mock_utils import add_users_to_db
 
 
 def main():
-
+    t0 = time.time()
     app = create_app()
     ctx = app.app_context()
     ctx.push()
     db.drop_all()
     db.create_all()
+    add_users(db)
+    print(f"DB setup in {time.time() - t0}s")
 
-    add_data(db)
 
-
-def add_data(db):
-    t0 = time()
+def add_users(db):
     print("adding users to db")
     add_users_to_db(db)
-    print(f"Db setup in {time() - t0}s")
 
 
 if __name__ == "__main__":
